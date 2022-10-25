94ed9f2d3d5d9235de59453d707382325e9b4e2f
jsomariano <junior_santos2100@outlook.com>
2022-10-24 23:09:56 -0300

    Adicionado antd e criado login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,11 +1,75 @@
-import * as React from 'react';
-
+import { Button, Checkbox, Col, Form, Input, Row } from 'antd';
+import { Content } from 'antd/lib/layout/layout';
+import React from 'react';
 import "./Login.css"
 
-export default function Login() {
+const App = () => {
+    const onFinish = (values) => {
+        console.log('Success:', values);
+    };
+    const onFinishFailed = (errorInfo) => {
+        console.log('Failed:', errorInfo);
+    };
     return (
-        <div class="LoginContainer">
-            Meu login
-        </div>
+        <Content className='ContentContainer'>
+            <Row>
+                <Col align="center" span={24}>
+                    <Form
+                        name="basic"
+                        className='Form'
+                        onFinish={onFinish}
+                        onFinishFailed={onFinishFailed}
+                        autoComplete="off"
+                    >
+                        <Form.Item
+                            className='LoginImageContainer'
+                        >
+                            <object data='./engineering_team.svg' width={400} height={400} />
+                        </Form.Item>
+
+                        <Form.Item
+                            label="E-mail"
+                            name="username"
+                            rules={[
+                                {
+                                    required: true,
+                                    message: 'Informe seu endereço de E-mail!',
+                                },
+                            ]}
+                        >
+                            <Input />
+                        </Form.Item>
+
+                        <Form.Item
+                            label="Senha"
+                            name="password"
+                            rules={[
+                                {
+                                    required: true,
+                                    message: 'Informe sua senha!',
+                                },
+                            ]}
+                        >
+                            <Input.Password />
+                        </Form.Item>
+
+                        <Form.Item
+                            name="remember"
+                            valuePropName="checked"
+                        >
+                            <Checkbox>Lembrar senha</Checkbox>
+                        </Form.Item>
+
+                        <Form.Item
+                        >
+                            <Button type="primary" htmlType="submit">
+                                Submit
+                            </Button>
+                        </Form.Item>
+                    </Form>
+                </Col>
+            </Row>
+        </Content >
     );
-}
+};
+export default App;
